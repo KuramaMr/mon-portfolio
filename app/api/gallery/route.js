8b0d169d73fd6d15a8cedd0863ecfc6ee8f2c5db
Dorian Maquet <takenzmr@gmail.com>
2024-07-30 15:45:50 +0200

    Essaie image

--- app/api/gallery/route.js
+++ app/api/gallery/route.js
@@ -1,8 +1,8 @@
 import { NextResponse } from 'next/server';
 
 const galleryData = [
-  { id: 1, titre: "Image 1", publicId: "sample1" },
-  { id: 2, titre: "Image 2", publicId: "sample2" },
+  { id: 1, titre: "cld-sample-4", publicId: "cld-sample-4" },
+  { id: 2, titre: "cld-sample-5", publicId: "cld-sample-5" },
   // Ajoutez d'autres images ici
 ];
 
