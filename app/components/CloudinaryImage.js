13683881a10ecd1a94ca7d11057025e514973778
Dorian Maquet <takenzmr@gmail.com>
2024-07-30 13:59:48 +0200

    Ajout de l'intégration Cloudinary

---
+++ app/components/CloudinaryImage.js
@@ -0,0 +1,13 @@
+import { CldImage } from 'next-cloudinary'
+
+export default function CloudinaryImage({ publicId, alt }) {
+  return (
+    <CldImage
+      width="400"
+      height="300"
+      src={publicId}
+      sizes="100vw"
+      alt={alt}
+    />
+  )
+}
--- app/galerie/page.js
+++ app/galerie/page.js
@@ -1,3 +1,15 @@
+import CloudinaryImage from '../components/CloudinaryImage';
+
 export default function Galerie() {
-    return <h1>Ma Galerie</h1>
-  }
+  // Ces IDs seront remplacés par vos vrais IDs d'images Cloudinary
+  const imageIds = ['sample', 'sample2'];
+
+  return (
+    <div>
+      <h1>Ma Galerie</h1>
+      {imageIds.map((id) => (
+        <CloudinaryImage key={id} publicId={id} alt={`Image ${id}`} />
+      ))}
+    </div>
+  );
+}
